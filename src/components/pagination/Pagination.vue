4d639633296187564c763ea5463ad5731336d47c
juanjinario <juanjco2@gmail.com>
2023-02-01 20:20:03 +0100

    feat: add disabled to next and previous buttonbs

--- src/components/pagination/Pagination.vue
+++ src/components/pagination/Pagination.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="btn-group" role="group" aria-label="Basic example">
-    <button type="button" class="btn btn-outline-primary" @click="onPrevious()">
+    <button
+      type="button"
+      class="btn btn-outline-primary"
+      :disabled="isDisabledPrevious"
+      @click="onPrevious()"
+    >
       Anterior
     </button>
-    <button type="button" class="btn btn-outline-primary" @click="onNext()">
-      Siguiete
+    <button
+      type="button"
+      class="btn btn-outline-primary"
+      :disabled="isDisabledNext"
+      @click="onNext()"
+    >
+      Siguiente
     </button>
   </div>
 </template>
 <script setup>
 import { computed } from "vue";
+const firstPage = 0;
+const lastPage = 9;
 
 const props = defineProps({
   page: {
     type: Number,
     default: 0,
   },
+});
+
+// computed
+const isDisabledPrevious = computed(() => {
+  return props.page === firstPage ? true : false;
+});
+
+const isDisabledNext = computed(() => {
+  return props.page === lastPage ? true : false;
 });
 
 // methods
